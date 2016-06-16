4d79b93e63c78aa52da83dbeaebf1a55c29d0ea0
heiheiya <122619445@qq.com>
2016-06-16 15:04:01 +0800

    add Geometry shader3
    Signed-off-by: heiheiya <122619445@qq.com>

---
+++ Advanced OpenGL/Geometry Shader3/Geometry Shader/Model.h
@@ -0,0 +1,31 @@
+#ifndef _MODEL_H_
+#define _MODEL_H_
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include "Mesh.h"
+
+class Model
+{
+public:
+	Model(GLchar* path)
+	{
+		this->loadModel(path);
+	}
+	void Draw(Shader shader);
+
+private:
+	void loadModel(string path);
+	void processNode(aiNode* node, const aiScene* scene);
+	Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+	vector<Texture> loadMaterialTexture(aiMaterial* mat, aiTextureType type, string typeName);
+private:
+	vector<Mesh> meshes;
+	string directory;
+	vector<Texture> textures_loaded;
+};
+
+GLint TextureFromFile(const char* path, string directory);
+
+#endif // !_MODEL_H_
